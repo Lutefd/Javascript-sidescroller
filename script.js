f525c2b0d1dfd4aaad1951d73658f08c74c2f6d0
Lutefd <lfaddourado@gmail.com>
2022-08-15 10:23:06 -0300

    basic key input handler done

--- script.js
+++ script.js
@@ -4,10 +4,35 @@ window.addEventListener(`load`, function () {
   canvas.width = 800;
   canvas.height = 720;
   //classes
-  class InputHandler {}
+  class InputHandler {
+    constructor() {
+      this.keys = [];
+      window.addEventListener('keydown', (e) => {
+        if (
+          e.key === 'ArrowDown' ||
+          e.key === 'ArrowUp' ||
+          e.key === 'ArrowLeft' ||
+          (e.key === 'ArrowRight' && this.keys.indexOf(e.key) === -1)
+        ) {
+          this.keys.push(e.key);
+        }
+      });
+      window.addEventListener('keyup', (e) => {
+        if (
+          e.key === 'ArrowDown' ||
+          e.key === 'ArrowUp' ||
+          e.key === 'ArrowLeft' ||
+          e.key === 'ArrowRight'
+        ) {
+          this.keys.splice(this.keys.indexOf(e.key), 1);
+        }
+      });
+    }
+  }
   class Player {}
   class Background {}
   class Enemy {}
+  const input = new InputHandler();
   //functions
   function handleEnemies() {}
   function displayStatusText() {}
